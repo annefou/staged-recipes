a076630edeb4d494deb4bfdbdbc5ef7aee3a6140
Anne Fouilloux <annefou@geo.uio.no>
2020-10-12 20:33:32 +0000

    fix .config file for CTSM/FATES

--- recipes/fates/build.sh
+++ recipes/fates/build.sh
@@ -3,10 +3,13 @@
 sed -i.bak "s/'checkout'/'checkout', '--trust-server-cert'/" ./manage_externals/manic/repository_svn.py
 ./manage_externals/checkout_externals
 
+cp .config_files.xml  ${PREFIX}/.
+
 mkdir -p ${PREFIX}/bin
 cp -r cime ${PREFIX}/
 cp -r cime_config ${PREFIX}/
 cp -r components ${PREFIX}/
+cp -r src ${PREFIX}/
 
 cd ${PREFIX}/bin/
 ln -s ../cime/scripts/create_* .
