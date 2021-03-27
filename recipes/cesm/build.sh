fc93116dc9d95efc788c2e7e3ae77689bff8abf0
Anne Fouilloux <annefou@geo.uio.no>
2021-03-27 20:29:08 +0000

    add perl XML-LIBXML

--- recipes/cesm/build.sh
+++ recipes/cesm/build.sh
@@ -13,3 +13,12 @@ ln -s ../cime/scripts/create_* .
 ln -s ../cime/scripts/query_* .
 
 sed -i.bak 's/# ARCHIVE COMMAND SIMILAR ACROSS ALL PLATFORMS/AR="$AR cq"/' ${PREFIX}/cime/src/externals/mct/configure
+sed -i.bak 's/# ARCHIVE COMMAND SIMILAR ACROSS ALL PLATFORMS/AR="$AR cq"/' ${PREFIX}/cime/src/externals/mct/mpi-serial/configure
+
+# add espress machines & associated compilers
+patch  ${PREFIX}/cime/config/cesm/machines/config_machines.xml ${RECIPE_DIR}/config_machines_xml.patch
+patch  ${PREFIX}/cime/config/cesm/machines/config_compilers.xml ${RECIPE_DIR}/config_compilers_xml.patch
+
+# install XML::LibXML
+export PERL_MM_USE_DEFAULT=1
+cpan XML::LibXML
---
+++ recipes/cesm/test-cesm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export NETCDF_DIR=$(nc-config --prefix)
+export CIME_MODEL=cesm
+export CESM_DATA_ROOT=/tmp
+
+mkdir -p /tmp/inputdata
+
+create_newcase --case /tmp/cases/F2000climo  --compset F2000climo --res f19_g17 --machine espresso --run-unsupported
+
+cd /tmp/cases/F2000climo
+./case.setup
+./case.build
+
+cd -
+
+rm -rf /tmp/cases/FF2000climo
