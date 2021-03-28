60c6d54641c4cbf2a9b373a51d173e5eb79b9a46
Anne Fouilloux <annefou@geo.uio.no>
2021-03-28 11:57:28 +0000

    fix removal folder when testing: there is an additional sub-folder depending on the model; here cesm

--- recipes/cesm/test-cesm.sh
+++ recipes/cesm/test-cesm.sh
@@ -3,16 +3,18 @@ set -e
 
 export NETCDF_DIR=$(nc-config --prefix)
 export CIME_MODEL=cesm
-export CESM_DATA_ROOT=/tmp
+export CESM_DATA_ROOT=$HOME
+export CESM_WORK_ROOT=$HOME
 
-mkdir -p /tmp/inputdata
+mkdir -p $CESM_DATA_ROOT/inputdata
 
-create_newcase --case /tmp/cases/F2000climo  --compset F2000climo --res f19_g17 --machine espresso --run-unsupported
+create_newcase --case $CESM_WORK_ROOT/cases/F2000climo  --compset F2000climo --res f19_g17 --machine espresso --run-unsupported
 
-cd /tmp/cases/F2000climo
+cd $CESM_WORK_ROOT/cases/F2000climo
 ./case.setup
 ./case.build
 
 cd -
 
-rm -rf /tmp/cases/F2000climo
+rm -rf $CESM_WORK_ROOT/cases/F2000climo
+rm -rf $CESM_WORK_ROOT/$CIME_MODEL/work/F2000climo
